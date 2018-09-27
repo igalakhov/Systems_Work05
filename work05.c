870c7348696612ec6f19455e1640fd59d6be6223
Ivan Galakhov <igalakhov00@stuy.edu>
2018-09-27 11:47:44 -0400

    abc

---
+++ work05.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+// strlen
+int my_strlen(char *s){
+
+  return 0;
+}
+
+//strcpy
+char * my_strcpy( char *dest, char *source){
+  char d;
+  return &d;
+}
+
+//strncpy
+char * strncpy( char *dest, char *source, int n){
+  char d;
+  return &d;
+}
+
+int main(){
+
+  return 0;
+}
