066d0c50ebfb45b0756f34eb63d1334ee79c36e9
Ivan Galakhov <igalakhov00@stuy.edu>
2018-09-28 11:44:40 -0400

    stuff but it's still broken

--- work05.c
+++ work05.c
@@ -25,22 +25,22 @@ char * my_strcat(char *dest, char *source ){
   return NULL;
 }
 
-// strncat
+// strncat - Ivan
 char * my_strncat(char *dest, char *source, int n){
   return NULL;
 }
 
-// strcmp
+// strcmp - Ivan
 int my_strcmp(char *s1, char *s2){
   return 0;
 }
 
-// strchr
+// strchr - Ivan
 char * my_strchr(char *s, char c){
   return NULL;
 }
 
-// strstr
+// strstr - Ivan
 char * my_strstr(char *s1, char *s2){
   return NULL;
 }
@@ -49,14 +49,14 @@ char * my_strstr(char *s1, char *s2){
 void reset_strings(char *s1, char *s2, char*s3){
   printf("Resetting string values... \n");
   strcpy(s1, "Hello");
-  strcpy(s2, "Wolrd");
+  strcpy(s2, "World");
   strcpy(s3, "Whatever");
 }
 void print_strings(char *s1, char *s2, char *s3){
   printf("Current string values: \n");
   printf("s1: [%s] \n", s1);
   printf("s2: [%s] \n", s2);
-  printf("s3: [%s] \n", s2);
+  printf("s3: [%s] \n", s3);
 }
 
 int main(){
@@ -64,7 +64,17 @@ int main(){
   reset_strings(s1, s2, s3);
   print_strings(s1, s2, s3);
 
-  
+  //strlen test
+  printf("\nTesting strlen(s1): \n");
+  printf("standart: %lu \n", strlen(s1));
+  printf("custom: %d \n", my_strlen(s1));
+
+  //strcopy test
+  printf("\nTesting strcopy(s1, s2): \n");
+  printf("standart: [%s]", strcopy(s1, s2));
+  printf("custom: [%d]", my_strcopy(s1, s2));  
+
+
 
 
   return 0;
